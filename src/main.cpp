32b6390dc904ca8474cf42f4af17012b421993e1
Jovan Svorcan <JovanS@users.noreply.github.com>
2019-11-22 18:35:09 +0100

    Update main.cpp
    Some comment lines were removed

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,4 @@ void loop() {
   MDNS.update();
 }
 
-void wifiScan() {
-  // Serial.println("\n\nNetwork scan");
-  // int n = WiFi.scanNetworks(false, true);
-  // String ssid;
-  // uint8_t encryptionType;
-  // int32_t RSSI;
-  // uint8_t* BSSID;
-  // int32_t channel;
-  // bool isHidden;
-
-  // for (int i = 0; i < n; i++)
-  // {
-  //   WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
-  //   Serial.printf("%d: %s, Ch:%d (%ddBm) %s %s\n", i + 1, ssid.c_str(), channel, RSSI, encryptionType == ENC_TYPE_NONE ? "open" : "", isHidden ? "hidden" : "");
-  // }
-  // Serial.println();
-}
-
-
 
